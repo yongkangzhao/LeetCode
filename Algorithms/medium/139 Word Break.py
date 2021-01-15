6bf7354c8ff06aa45646a69c29ed63fae8be0bb6
Yongkang Zhao <cookieyong@gmail.com>
2021-01-14 20:48:43 -0800

    new 139 solution

--- Algorithms/medium/139 Word Break.py
+++ Algorithms/medium/139 Word Break.py
@@ -21,7 +21,7 @@ Example 3:
 Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
 Output: false
 '''
-
+# Solution 1
 from typing import List
 
 class Solution:
@@ -41,4 +41,29 @@ class Solution:
                 if(self.dfs(s[len(word):])):
                     return True
         self.memo.add(s)
+        return False
+
+# Solution 2
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        self.words = set(wordDict)
+        self.startDict = collections.defaultdict(set)
+        for word in wordDict:
+            self.startDict[word[0]].add(word)
+        self.memo = {}
+        return self.dfs(s)
+        
+    def dfs(self, s):
+        if s in self.memo: return self.memo[s]
+        if s in self.words: return True
+        result = False
+        if s[:1] in self.startDict:
+            prefixes = self.startDict[s[0]]
+            for prefix in prefixes:
+                if prefix == s[:len(prefix)]:
+                    result = self.dfs(s[len(prefix):])
+                if result:
+                    self.memo[s] = True
+                    return True
+            self.memo[s] = False
         return False
