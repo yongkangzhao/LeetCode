952a17e9f46497517ff032d36c2641225b56e268
Yongkang Zhao <cookieyong@gmail.com>
2021-01-19 08:08:16 -0800

    921

---
+++ Algorithms/medium/921 Minimum Add to Make Parentheses Valid.py
@@ -0,0 +1,43 @@
+'''
+Given a string S of '(' and ')' parentheses, we add the minimum number of parentheses ( '(' or ')', and in any positions ) so that the resulting parentheses string is valid.
+
+Formally, a parentheses string is valid if and only if:
+
+It is the empty string, or
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+Given a parentheses string, return the minimum number of parentheses we must add to make the resulting string valid.
+
+ 
+
+Example 1:
+
+Input: "())"
+Output: 1
+Example 2:
+
+Input: "((("
+Output: 3
+Example 3:
+
+Input: "()"
+Output: 0
+Example 4:
+
+Input: "()))(("
+Output: 4
+'''
+
+class Solution:
+    def minAddToMakeValid(self, S: str) -> int:
+        front = 0
+        p_count = 0
+        for p in S:
+            if p == '(':
+                p_count += 1
+            else:
+                p_count -= 1
+                if p_count == -1:
+                    front += 1
+                    p_count = 0
+        return front + p_count
