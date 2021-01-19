c59a68e6288a8b821abd1e82c9faacd3ead18377
Yongkang Zhao <cookieyong@gmail.com>
2021-01-19 07:51:33 -0800

    1021

---
+++ Algorithms/easy/1021 Remove Outermost Parentheses.py
@@ -0,0 +1,50 @@
+'''
+A valid parentheses string is either empty (""), "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.  For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+
+A valid parentheses string S is primitive if it is nonempty, and there does not exist a way to split it into S = A+B, with A and B nonempty valid parentheses strings.
+
+Given a valid parentheses string S, consider its primitive decomposition: S = P_1 + P_2 + ... + P_k, where P_i are primitive valid parentheses strings.
+
+Return S after removing the outermost parentheses of every primitive string in the primitive decomposition of S.
+
+ 
+
+Example 1:
+
+Input: "(()())(())"
+Output: "()()()"
+Explanation: 
+The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+Example 2:
+
+Input: "(()())(())(()(()))"
+Output: "()()()()(())"
+Explanation: 
+The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+Example 3:
+
+Input: "()()"
+Output: ""
+Explanation: 
+The input string is "()()", with primitive decomposition "()" + "()".
+After removing outer parentheses of each part, this is "" + "" = "".
+'''
+
+class Solution:
+    def removeOuterParentheses(self, S: str) -> str:
+        depth_counter = 0
+        start = 1
+        result = []
+        for i in range(len(S)):
+            if S[i] == '(':
+                depth_counter += 1
+            else:
+                depth_counter -= 1
+                if depth_counter == 0:
+                    result += [S[start:i]]
+                    start = i+2
+        return ''.join(result)
+
+## didn't make result as a string, since string is imuatable, and every iteration would cause a new string to be created, thus it is slower than concantenation in the end from a list.
